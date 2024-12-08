07dc067f8a1cbd20a15932909692a25c38de3735
William <94484815+WinningWillow04@users.noreply.github.com>
2024-12-08 13:14:22 -0500

    start

---
+++ finalproject_application_1/lib/page/leaderboard.dart
@@ -0,0 +1,21 @@
+// Import to detect if the application is running on the web platform
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+// Import to use Flutter's core framework and material design widgets
+import 'package:flutter/material.dart';
+
+// Import to access and control the device camera
+import 'package:camera/camera.dart';
+
+// Import to access commonly used locations on device's file system
+import 'package:path_provider/path_provider.dart';
+
+// Import for file operations, sockets, HTTP, and other I/O-related tasks (for mobile platforms)
+import 'dart:io';
+
+// Import for handling binary data efficiently 
+import 'dart:typed_data'; 
+
+// Import for platform-specific services and interaction with native code
+import 'package:flutter/services.dart';
+
